742c3a5291c8cf3139bbeae7951c36c19aa3494e
alan3700 <alandaunay1@gmail.com>
2022-12-20 10:27:14 +0100

    Signin test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
+import Signin from "./components/Signin/Signin";
 import {
   ApolloClient,
   InMemoryCache,
@@ -7,67 +8,20 @@ import {
   useQuery,
   useMutation,
 } from "@apollo/client";
-import { createUser } from "./graphql/createUser";
-
-function Signup() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const [doSignupMutation, { data, loading, error }] = useMutation(createUser);
-
-  async function doSignup() {
-    try {
-      await doSignupMutation({
-        variables: {
-          data: {
-            email,
-            password,
-          },
-        },
-      });
-      setEmail("");
-      setPassword("");
-    } catch {}
-  }
-
-  return (
-    <>
-      {error && (
-        <pre style={{ color: "red" }}>{JSON.stringify(error, null, 4)}</pre>
-      )}
-      <p>Email:</p>
-      <input
-        disabled={loading}
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <p>Password:</p>
-      <input
-        disabled={loading}
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button disabled={loading} onClick={doSignup}>
-        Signup
-      </button>
-    </>
-  );
-}
-
-function Main() {
-  return (
-    <div>
-      <Signup />
-    </div>
-  );
-}
 
 const client = new ApolloClient({
   uri: "http://localhost:5000",
   cache: new InMemoryCache(),
 });
+
+function Main() {
+  return (
+    <div>
+      <Signin />
+    </div>
+  );
+}
+
 
 function App() {
   return (
---
+++ src/components/Signin/signin.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react"
+import Signin from "./Signin";
+import { MockedProvider } from "@apollo/client/testing";
+import { createUser } from "../../graphql/createUser";
+
+const mocks = [
+    {
+        request: {
+          query: createUser,
+        },
+    }
+]
+
+describe('Signin', ()=>{
+    it('renders a button', ()=>{
+        render(
+            <MockedProvider mocks={mocks} addTypename={false}>
+                <Signin/>
+            </MockedProvider>
+        )
+        expect(screen.getByRole('button')).toBeInTheDocument()
+    })
+})
+
