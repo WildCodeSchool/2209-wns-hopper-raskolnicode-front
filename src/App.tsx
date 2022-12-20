2be09e4b9a6d7e6b45d13b9430dbc465a1b2d515
feliciebernard <felicie.bernard@outlook.fr>
2022-12-20 15:17:40 +0100

    router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Signup from "./components/Signup/Signup";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-} from "@apollo/client";
+import Blogs from "./pages/Blogs";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
 
 const client = new ApolloClient({
   uri: "http://localhost:5000",
@@ -14,12 +14,17 @@ const client = new ApolloClient({
 
 function Main() {
   return (
-    <div>
-      <Signup />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/blogs" element={<Blogs />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 
 function App() {
   return (
