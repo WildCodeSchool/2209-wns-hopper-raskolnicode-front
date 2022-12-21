e30c28d4feef1f70bf1876252f5365fbc36c4732
alan3700 <alandaunay1@gmail.com>
2022-12-21 16:51:58 +0100

    navbar first commit

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
 
 const Layout = () => {
   return (
     <>
+      <Navbar />
+      <hr />
       <Outlet />
     </>
   );
