f6f1af1f97d3433ffd6791ab8a6e0d57fdf59c89
alan3700 <alandaunay1@gmail.com>
2022-12-22 10:29:59 +0100

    ajout style login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useMutation } from "@apollo/client";
-import styles from "../Signup/Signup.module.scss";
+import styles from "./Login.module.scss";
 import { SIGN_IN } from "../../graphql/mutations";
 import { IUser } from "../../interfaces";
 import { useNavigate } from "react-router-dom";
@@ -37,10 +37,9 @@ function Login(props: {
   }
 
   return (
-    <main className={styles.signupMain}>
-      <div className={styles.form}>
+    <main className={styles.logingMain}>
+        <form onSubmit={doSignIn} className={styles.form}>
         <h3>Connexion</h3>
-        <form onSubmit={doSignIn}>
           <div className={styles.email}>
             <input
               disabled={loading}
@@ -67,13 +66,13 @@ function Login(props: {
               Me connecter
             </button>
           </div>
-        </form>
-        <div>
+          <div>
           <p>
             Avez-vous déja un <a href="X">compte?</a>
           </p>
         </div>
-      </div>
+        </form>
+        
     </main>
   );
 }
