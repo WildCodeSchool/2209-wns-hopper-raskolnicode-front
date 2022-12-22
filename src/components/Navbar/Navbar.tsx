06cffba30888e0f869b355ffd2c930aa1ec670f2
feliciebernard <felicie.bernard@outlook.fr>
2022-12-22 16:00:23 +0100

    fixed

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,20 @@
-import { Link } from 'react-router-dom';
-import styles from './Navbar.module.scss'
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.scss";
 function Navbar() {
-
-
   return (
     <main className={styles.navbarmain}>
-        <Link className={styles.linklogo} to={'/'}><img className={styles.logo} src="./logo.png" alt="" /></Link>
-        
-        <div className={styles.link}>
-        <Link className={styles.linknav} to={'/signup'}>Inscription</Link>
-        <Link className={styles.linknav} to={'/login'}>Connection</Link>
-        </div>
-        
+      <Link className={styles.linklogo} to={"/"}>
+        <img className={styles.logo} src="./logo.png" alt="" />
+      </Link>
+
+      <div className={styles.link}>
+        <Link className={styles.linknav} to={"/signup"}>
+          Inscription
+        </Link>
+        <Link className={styles.linknav} to={"/login"}>
+          Connection
+        </Link>
+      </div>
     </main>
   );
 }
