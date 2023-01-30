991aa603cfc4cebb2b3c37de0110043d47fac65d
feliciebernard <felicie.bernard@outlook.fr>
2023-01-30 12:24:02 +0100

    some fixes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,27 +1,37 @@
-import { Link } from 'react-router-dom';
-import styles from './Navbar.module.scss'
-import { useContext } from 'react';
-import { UserContext } from '../../UserContext';
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.scss";
+import { useContext } from "react";
+import { UserContext } from "../../UserContext";
 
-function Navbar(props: {onTokenChange: () => void}) {
-  const user = useContext(UserContext)
+function Navbar(props: { onTokenChange: () => void }) {
+  const user = useContext(UserContext);
 
   return (
     <main className={styles.navbarmain}>
-        <Link className={styles.linklogo} to={'/'}><img className={styles.logo} src="./blackLogo.png" alt="" /></Link>
-        
-        <div className={styles.link}>
-          {
-            user ?
-            <Link className={styles.linknav} to={''} onClick={() => props.onTokenChange()}>Déconnexion</Link>
-            :
-            <>
-            <Link className={styles.linknav} to={'/signup'}>Inscription</Link>
-            <Link className={styles.linknav} to={'/login'}>Connexion</Link>
-            </>
-          }
+      <Link className={styles.linklogo} to={"/"}>
+        <img className={styles.logo} src="./logo.png" alt="" />
+      </Link>
 
-        </div>
+      <div className={styles.link}>
+        {user ? (
+          <Link
+            className={styles.linknav}
+            to={""}
+            onClick={() => props.onTokenChange()}
+          >
+            Déconnexion
+          </Link>
+        ) : (
+          <>
+            <Link className={styles.linknav} to={"/signup"}>
+              Inscription
+            </Link>
+            <Link className={styles.linknav} to={"/login"}>
+              Connexion
+            </Link>
+          </>
+        )}
+      </div>
     </main>
   );
 }
