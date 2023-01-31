40b48baa18f3539eaa208458a22d8332d5041516
feliciebernard <felicie.bernard@outlook.fr>
2023-01-31 16:26:00 +0100

    card added

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styles from "./Card.module.scss";
+
+interface IProps {
+  image: string;
+  title: string;
+  description: string;
+  updated_at: string;
+}
+
+function Card(props: IProps): JSX.Element {
+  return (
+    <section className={styles.articlelist}>
+      <div className={styles.listCard}>
+        <div className={styles.card}>
+          <img
+            src="https://picsum.photos/1200/400?random=2"
+            alt={props.title}
+          />
+          <h4>{props.title}</h4>
+          <p>{props.description}</p>
+          <p className={styles.dateCreated}>{props.updated_at}</p>
+        </div>
+        <div className={styles.card}>
+          <img
+            src="https://picsum.photos/1200/400?random=2"
+            alt={props.title}
+          />
+          <h4>{props.title}</h4>
+          <p>{props.description}</p>
+          <p className={styles.dateCreated}>{props.updated_at}</p>
+        </div>
+        <div className={styles.card}>
+          <img
+            src="https://picsum.photos/1200/400?random=2"
+            alt={props.title}
+          />
+          <h4>{props.title}</h4>
+          <p>{props.description}</p>
+          <p className={styles.dateCreated}>{props.updated_at}</p>
+        </div>
+        <div className={styles.card}>
+          <img
+            src="https://picsum.photos/1200/400?random=2"
+            alt={props.title}
+          />
+          <h4>{props.title}</h4>
+          <p>{props.description}</p>
+          <p className={styles.dateCreated}>{props.updated_at}</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Card;
