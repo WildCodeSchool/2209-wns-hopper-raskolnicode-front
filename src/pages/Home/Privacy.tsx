7d30d10c3d07f7c43498092dd4222576797a6654
feliciebernard <felicie.bernard@outlook.fr>
2023-02-01 10:23:49 +0100

    privacy page

--- src/pages/Home/Privacy.tsx
+++ src/pages/Home/Privacy.tsx
@@ -3,96 +3,127 @@ import styles from "./Home.module.scss";
 
 function Privacy() {
   return (
-    <p>
-      Starblog, en sa qualité de responsable du traitement de vos données,
-      attache une grande importance à la protection et au respect de votre vie
-      privée. En effet, nous estimons que vous devriez toujours savoir quelles
-      sont les données que nous recueillons de votre part et comment nous les
-      utilisons. La présente politique vise à vous informer de nos pratiques
-      concernant la collecte, l'utilisation et le droit d'accès à ces données
-      que vous êtes amenés à nous fournir par le biais de nos Services. Cette
-      politique, ainsi que nos Conditions générales d'utilisation, présente la
-      manière dont nous traitons les données personnelles que vous nous
-      fournissez ou que nous recueillons. Les données de SKYROCK.com sont
-      stockées sur des serveurs en FRANCE et relèvent du droit français et a
-      fortiori du droit européen. A cet égard les fichiers SKYROCK.com ont fait
-      l'objet d'une déclaration auprès de la CNIL et sont en conformité avec les
-      dispositions du Règlement Général pour la protection des données (RGPD).
-      Nous vous invitons à lire attentivement le présent document pour connaître
-      et comprendre nos pratiques quant aux traitements de vos données
-      personnelles que nous mettons en œuvre. Les informations que nous
-      recueillons : Nous sommes susceptibles de recueillir et de traiter les
-      données suivantes : Les informations que vous nous transmettez directement
-      : En utilisant nos Services, vous êtes amenés à nous transmettre des
-      informations, dont certaines sont de nature à vous identifier, telles
-      qu'une photographie, vos prénom et nom, une mini biographie, votre
-      adresse, votre position GPS si vous l'avez autorisée... C'est notamment le
-      cas lorsque vous remplissez des formulaires (comme par exemple le
-      formulaire d'inscription), lorsque vous participez à l'un de nos jeux,
-      concours, offres promotionnelles, études ou sondages, lorsque vous nous
-      contactez - que ce soit par formulaire, email ou tout autre moyen de
-      communication - ou lorsque vous nous faites part d'un problème. Ces
-      données sont nécessaires à la fourniture du service. Ces données sont
-      notamment vos nom et prénom, adresse e-mail, date de naissance, sexe,
-      numéro de téléphone et mot de passe. Ces informations sont obligatoires. A
-      défaut, SKYROCK.com ne sera pas en mesure de vous fournir les services
-      attendus et vous ne serez pas autorisés à créer de compte sur notre
-      Service. Les données que nous recueillons automatiquement : Lors de
-      chacune de vos visites, nous sommes susceptibles de recueillir,
-      conformément à la législation applicable, des informations relatives aux
-      appareils sur lesquels vous utilisez nos services ou aux réseaux depuis
-      lesquels vous accédez à nos services, tels que notamment vos adresses IP,
-      vos données de connexion, les données concernant votre parcours de
-      navigation sur notre Service, le contenu auquel vous accédez ou que vous
-      consultez, les termes de recherches utilisés, les erreurs de
-      téléchargement, la durée de votre consultation, l'identifiant publicitaire
-      de votre appareil... Dans le cas où vous vous connectez à nos Services en
-      utilisant les fonctionnalités de réseaux sociaux mises à votre
-      disposition, SKYROCK.com aura accès à certaines des données (notamment,
-      vos prénom, nom de famille, photographie, adresse e-mail) transmises par
-      le réseau social conformément aux conditions générales d'utilisation du
-      réseau social concerné. Nous sommes également susceptibles de recueillir
-      certaines de vos Données Personnelles lorsque vous interagissez avec des
-      fonctionnalités de ces réseaux sociaux, tel que les fonctionnalités «
-      J'aime », dès lors que vous l'avez autorisé. Les destinataires de vos
-      données : Les données collectées sur nos Services et applications sont
-      exclusivement destinées à SKYROCK.com. Elles sont toutefois susceptibles
-      d'être transmises à des sociétés du Groupe opérant le Service, des
-      sociétés sous-traitantes (dans la limite nécessaire à l'accomplissement
-      des missions que nous leur confions) ou, sous réserve d'une autorisation
-      préalable de votre part, à des sociétés partenaires. Nous exigeons de nos
-      sous-traitants qu'ils appliquent des règles strictes en matière de
-      protection des données personnelles en conformité avec les dispositions du
-      Règlement Général pour la protection des données (RGPD). Durée de
-      conservation de vos données : Vos Données Personnelles sont effacées à
-      l'issue des périodes suivantes: - 14 mois après la dernière utilisation de
-      notre service, si vous n'avez pas fermé votre compte ; - 12 mois après la
-      fermeture de votre compte. Vos droits sur vos données personnelles : Vous
-      disposez du droit de recevoir une copie de vos Données Personnelles en
-      notre possession (« droit d'accès »). Vous pouvez également demander
-      l'effacement de vos Données Personnelles ainsi que la rectification des
-      Données Personnelles erronées ou obsolètes (« droit d'effacement et droit
-      de rectification »). Veuillez noter que nous sommes susceptibles de
-      conserver certaines informations vous concernant lorsque la loi nous
-      l'impose ou lorsque nous avons un motif légitime de le faire. C'est par
-      exemple le cas, si nous estimons que vous avez commis une fraude ou violé
-      nos Conditions Générales d'Utilisation et que nous souhaitons éviter que
-      vous contourniez les règles applicables à notre communauté. Vous disposez
-      également du droit de vous opposer à tout moment pour des raisons tenant à
-      sa situation particulière, (i) au traitement de vos Données Personnes à
-      des fins de marketing direct ou aux traitements effectués sur le fondement
-      de notre intérêt légitime (« droit d'opposition »). Vous disposez
-      également du droit à la portabilité de vos données, c'est-à-dire au droit
-      de recevoir les Données Personnelles que vous nous avez fournis dans un
-      format structuré, couramment utilisé et lisible par la machine et le droit
-      de transmettre ces données à un autre responsable du traitement (« droit à
-      la portabilité »). Pour exercer ces droits, vous pouvez contacter notre
-      Délégué à la Protection des Données Personnelles à l'adresse email
-      suivante : dpo@skyrock.com ou en nous adressant un courrier à l'adresse
-      suivante : SKYROCK.com - A l'attention du Délégué à la Protection des
-      Données - 37 bis rue Greneta - 7500 Paris - France Version mise à jour le
-      25 mai 2018
-    </p>
+    <main className={styles.homeMain}>
+      <div className={styles.bodyPrivacy}>
+        <h1>Politique de confidentialité</h1>
+        <p>
+          <strong>Starblog</strong>, en sa qualité de responsable du traitement
+          de vos données, attache une grande importance à la protection et au
+          respect de votre vie privée. En effet, nous estimons que vous devriez
+          toujours savoir quelles sont les données que nous recueillons de votre
+          part et comment nous les utilisons. La présente politique vise à vous
+          informer de nos pratiques concernant la collecte, l&#x27;utilisation
+          et le droit d&#x27;accès à ces données que vous êtes amenés à nous
+          fournir par le biais de nos services.
+        </p>
+        <p>
+          Cette politique, ainsi que nos Conditions générales
+          d&#x27;utilisation, présente la manière dont nous traitons les données
+          personnelles que vous nous fournissez ou que nous recueillons. Les
+          données de <strong>Starblog</strong> sont stockées sur des serveurs en
+          dans des satellites français et relèvent du droit français et a
+          fortiori du droit européen. A cet égard les fichiers
+          <strong>Starblog</strong> feront l&#x27;objet d&#x27;une déclaration
+          auprès de la CNIL et sont en conformité avec les dispositions du
+          Règlement Général pour la protection des données (RGPD).
+        </p>
+        <p>
+          Nous vous invitons à lire attentivement le présent document pour
+          connaître et comprendre nos pratiques quant aux traitements de vos
+          données personnelles que nous mettons en œuvre.
+        </p>
+        <h2>
+          <strong>Les informations que nous recueillons</strong>
+        </h2>
+        <p>
+          Nous sommes susceptibles de recueillir et de traiter les données que
+          vous nous transmettez directement.
+        </p>
+        <p>
+          En utilisant nos Services, vous êtes amenés à nous transmettre des
+          informations, dont certaines sont de nature à vous identifier, telles
+          que des images, votre pseudonyme, votre adresse mail... C&#x27;est
+          notamment le cas lorsque vous remplissez des formulaires (comme par
+          exemple le formulaire d&#x27;inscription), ou lorsque vous nous
+          contactez - que ce soit par formulaire, email ou tout autre moyen de
+          communication.
+        </p>
+        <p>
+          Ces données sont nécessaires à la fourniture du service. Ces
+          informations sont obligatoires. A défaut, <strong>Starblog</strong> ne
+          sera pas en mesure de vous fournir les services attendus et vous ne
+          serez pas autorisés à créer de compte sur notre Service.
+        </p>
+        <h2>
+          <strong>Les données que nous recueillons automatiquement</strong>
+        </h2>
+        <p>
+          Lors de chacune de vos visites, nous sommes susceptibles de
+          recueillir, conformément à la législation applicable, des informations
+          relatives aux appareils sur lesquels vous utilisez nos services ou aux
+          réseaux depuis lesquels vous accédez à nos services, tels que
+          notamment vos adresses IP, vos données de connexion, les données
+          concernant votre parcours de navigation sur notre service, le contenu
+          auquel vous accédez ou que vous consultez, les termes de recherches
+          utilisés, les erreurs de téléchargement, la durée de votre
+          consultation, l&#x27;identifiant publicitaire de votre appareil...
+        </p>
+        <h2>
+          <strong>Les destinataires de vos données</strong>
+        </h2>
+        <p>
+          Les données collectées sur nos Services et applications sont
+          exclusivement destinées à <strong>Starblog</strong>.
+        </p>
+        <h2>
+          <strong>Durée de conservation de vos données</strong>
+        </h2>
+        <p>
+          Vos données personnelles sont effacées lorsque vous supprimez votre
+          compte
+        </p>
+        <h2>
+          <strong>Vos droits sur vos données personnelles</strong>
+        </h2>
+        <p>
+          Vous disposez du droit de recevoir une copie de vos données
+          personnelles en notre possession (« droit d&#x27;accès »). Vous pouvez
+          également demander l&#x27;effacement de vos données personnelles ainsi
+          que la rectification des données personnelles erronées ou obsolètes («
+          droit d&#x27;effacement et droit de rectification »). Veuillez noter
+          que nous sommes susceptibles de conserver certaines informations vous
+          concernant lorsque la loi nous l&#x27;impose ou lorsque nous avons un
+          motif légitime de le faire. C&#x27;est par exemple le cas, si nous
+          estimons que vous avez commis une fraude ou violé nos Conditions
+          Générales d&#x27;Utilisation et que nous souhaitons éviter que vous
+          contourniez les règles applicables à notre communauté.
+        </p>
+        <p>
+          Vous disposez également du droit de vous opposer à tout moment pour
+          des raisons tenant à sa situation particulière, au traitement de vos
+          Données Personnes à des fins de marketing direct ou aux traitements
+          effectués sur le fondement de notre intérêt légitime (« droit
+          d&#x27;opposition »). Vous disposez également du droit à la
+          portabilité de vos données, c&#x27;est-à-dire au droit de recevoir les
+          Données Personnelles que vous nous avez fournis dans un format
+          structuré, couramment utilisé et lisible par la machine et le droit de
+          transmettre ces données à un autre responsable du traitement (« droit
+          à la portabilité »).
+        </p>
+        <p>
+          Pour exercer ces droits, vous pouvez contacter notre Délégué à la
+          Protection des Données Personnelles à l&#x27;adresse email suivante :
+          dpo@yopmail.com ou en nous adressant un courrier à l&#x27;adresse
+          suivante : <strong>Starblog</strong> - A l&#x27;attention du Délégué à
+          la Protection des Données - 404 rue des Etoiles - 54321 Starville -
+          France
+        </p>
+        <br />
+        <p>
+          <i>Version mise à jour le 30 janvier 2023.</i>
+        </p>
+      </div>
+    </main>
   );
 }
 
