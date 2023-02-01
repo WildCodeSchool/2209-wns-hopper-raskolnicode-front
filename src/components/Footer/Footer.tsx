76f6c32c340407ab86933d2fb5fcb4306a2ff9cf
Vehementz <vale-tudo91@sfr.fr>
2023-02-01 14:33:00 +0100

    footer resolved and responsive

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,10 +5,11 @@ function Footer() {
 
   return (
     <footer className={styles.footermain}>
+      <div className={styles.logoImageBox}>
         <Link className={styles.linklogo} to={'/'}><img className={styles.logo} src="./whiteLogo.png" alt="" /></Link>
-        <p>Copyright © 2023 StarBlog, Inc.</p>
-
+      </div>
         <div className={styles.containerLink}>
+        <p className={styles.copyright}>Copyright © 2023 StarBlog, Inc.</p>
         <Link className={styles.linknav} to={'/mentions'}>Mentions légale</Link>
         <Link className={styles.linknav} to={'/politique'}>Politique de confidentialité</Link>
         </div>
