3bb38c0e2c99ac1a3aae8a4b92bc40ad8fdb4399
feliciebernard <felicie.bernard@outlook.fr>
2023-02-01 14:44:45 +0100

    privacy added

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,18 +1,23 @@
-import styles from './Footer.module.scss'
-import { Link } from 'react-router-dom';
+import styles from "./Footer.module.scss";
+import { Link } from "react-router-dom";
 
 function Footer() {
-
   return (
     <footer className={styles.footermain}>
       <div className={styles.logoImageBox}>
-        <Link className={styles.linklogo} to={'/'}><img className={styles.logo} src="./whiteLogo.png" alt="" /></Link>
+        <Link className={styles.linklogo} to={"/"}>
+          <img className={styles.logo} src="./whiteLogo.png" alt="" />
+        </Link>
       </div>
-        <div className={styles.containerLink}>
+      <div className={styles.containerLink}>
         <p className={styles.copyright}>Copyright © 2023 StarBlog, Inc.</p>
-        <Link className={styles.linknav} to={'/mentions'}>Mentions légale</Link>
-        <Link className={styles.linknav} to={'/politique'}>Politique de confidentialité</Link>
-        </div>
+        <Link className={styles.linknav} to={"/mentions"}>
+          Mentions légales
+        </Link>
+        <Link className={styles.linknav} to={"/privacy"}>
+          Politique de confidentialité
+        </Link>
+      </div>
     </footer>
   );
 }
