9be7d37fd5867b6270077c201d0eb217f3bc2093
Soufiane Aït <soufiane.ait@outlook.fr>
2023-03-30 14:59:48 +0200

    config.ts 3 urls

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,13 @@
-export const API_URL = window.location.href.includes("hopper2.wns.wilders.dev")
-  ? "https://api.hopper2.wns.wilders.dev"
-  : "http://localhost:5000";
+let api_url;
+
+if (window.location.href.includes("staging.hopper2")) {
+  api_url = "https://api-staging.hopper2.wns.wilders.dev";
+
+} else if (window.location.href.includes("hopper2")) {
+  api_url = "https://api.hopper2.wns.wilders.dev";
+  
+} else {
+  api_url = "http://localhost:5000";
+}
+
+export const API_URL = api_url
