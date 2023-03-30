d2cac5ac57ef10541bed08849358de739eb51771
Soufiane Aït <soufiane.ait@outlook.fr>
2023-03-30 12:08:12 +0200

    edit url api

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const API_URL = window.location.href.includes("hopper2.wns.wilders.dev")
-  ? "https://hopper2.wns.wilders.dev:5000"
+  ? "https://api.hopper2.wns.wilders.dev"
   : "http://localhost:5000";
