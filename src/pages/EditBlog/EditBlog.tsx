d2b6171152b4907a407d371bf6a5fa0a1b2226d3
Vehementz <vale-tudo91@sfr.fr>
2023-06-22 12:23:52 +0200

    edit blog bug when image not modified corrected

--- src/pages/EditBlog/EditBlog.tsx
+++ src/pages/EditBlog/EditBlog.tsx
@@ -27,8 +27,7 @@ function EditBlog() {
   const [description, setDescription] = useState('');
 
   const [picture, setPicture] = useState<IPicture | undefined>(undefined);
-  const [pictureInForm, setPictureInForm] = useState<any>(undefined)
-
+  const [pictureInForm, setPictureInForm] = useState<any>(picture);
 
 
   const { loading, error, data, refetch } = useQuery<{ getBlog: IBlog }>(GET_BLOG, {
@@ -38,20 +37,14 @@ function EditBlog() {
   });
 
 
-  
-
   const [doUpdateBlogMutation, { loading: updateLoading, error: updateError }] = useMutation(UPDATE_BLOG);
-
-
-
 
   async function doUpdateBlog(e: any) {
     e.preventDefault();
     let cloudinaryPicture = null
-    cloudinaryPicture = await uploadCloudinary(pictureInForm);
-
-
-
+    if (pictureInForm) {
+      cloudinaryPicture = await uploadCloudinary(pictureInForm);
+    }
     if (data?.getBlog) {
       await doUpdateBlogMutation({
         variables: {
@@ -59,25 +52,23 @@ function EditBlog() {
           data: {
             name: name,
             description: description,
-            picture: {
-              name: cloudinaryPicture?.original_filename,
-              link: cloudinaryPicture?.secure_url,
-            },
+            picture: cloudinaryPicture
+              ? {
+                  name: cloudinaryPicture?.original_filename,
+                  link: cloudinaryPicture?.secure_url,
+                }
+              : {
+                  // exclude __typename from picture data
+                  link: data.getBlog.picture.link,
+                  name: data.getBlog.picture.name,
+                }, // If no new picture is selected, use the existing picture
           },
         },
       }).then(res => {
-      navigate(`/blog/${getBlogId}/`)
-    })
-  
-      
+        navigate(`/blog/${getBlogId}/`)
+      })
     }
-    
   };
-
-
-
-
-
 
 
   useEffect(() => {
@@ -115,16 +106,15 @@ function EditBlog() {
 
           <UploadPicture setPictureInForm={setPictureInForm} picture={picture} />
 
-
           <input className={styles.blogName} name="name" value={name} onChange={(e) => setName(e.target.value)} />
           <textarea className={styles.blogDescription} name="description" value={description} onChange={(e) => setDescription(e.target.value)} />
 
           <div className={styles.dateBox}>
             <p className={`${styles.dateline} ${styles.createdDate}`}>
-              Created: {moment(data.getBlog.created_at).locale('fr').format('dddd D MMMM YYYY [à] HH[h]mm')}
+              Créer le: {moment(data.getBlog.created_at).locale('fr').format('dddd D MMMM YYYY [à] HH[h]mm')}
             </p>
             <p className={`${styles.dateline} ${styles.updatedDate}`}>
-              Last updated: {moment(data.getBlog.updated_at).locale('fr').format('dddd D MMMM YYYY [à] HH[h]mm')}
+              Dernière édition: {moment(data.getBlog.updated_at).locale('fr').format('dddd D MMMM YYYY [à] HH[h]mm')}
             </p>
           </div>
           <button className={styles.submitButton} type="submit">Mettre à jour</button>
